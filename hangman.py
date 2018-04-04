441578830f6c0951a69045456272f4d100d42ac8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 08:34:45 -0400

    Updated from Brython Server: 4/4/2018 8:34:44 AM

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@
 
 from ggame import *
 
-black=Color(0xffffff,1)
+black=Color(0x000000,1)
 brown=Color(0x934b14,1)
 
 blackoutline=LineStyle(1, black)
@@ -13,5 +13,5 @@ gallows1=RectangleAsset(30,1000,blackoutline,brown)
 gallows2=RectangleAsset(200,30,blackoutline,brown)
 
 Sprite(gallows1,(20,50))
-Sprite(gallows2, (40,20))
+Sprite(gallows2, (40,30))
 App().run()
