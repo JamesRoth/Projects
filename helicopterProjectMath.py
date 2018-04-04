01a2da2356ffbb91583f77e4a86239c31f62aacb
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 13:48:42 -0400

    Updated from Brython Server: 4/4/2018 1:48:40 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -5,4 +5,6 @@
 
 
 def elev(xl,yl,E,A,Z):
-    return((yl-sin(A)*Z*tan(E))**2 + (xl-cos(A)*Z*tan(E))**2)**1/2
+    return((yl-sin(A)*Z*tan(E))**2 + (xl-cos(A)*Z*tan(E))**2)**1/2)/z-61
+    
+print(elev(40.6,-106,77.6,23.1,408))
