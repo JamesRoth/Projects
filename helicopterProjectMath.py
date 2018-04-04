0e402d56a5fc3998b4cf9ad04ad31f2b1198fe2d
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 14:48:13 -0400

    Updated from Brython Server: 4/4/2018 2:48:11 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -12,7 +12,8 @@ def elev(xl,yl,E,A,Z):
     p1=round((sina*Z*tane-yl), 4)
     p2=round((cosa*Z*tane-xl),4)
     print(p1,p2,"SPACE")
-    ansrad=((((yl-sina*Z*tane)**2 + (xl-cosa*Z*tane))**2)**1/2)/(Z-61)
+    print(sqrt((p1**2+p2**2)),"SPACE")
+    ansrad=((sqrt(p1**2 + p2**2)/Z-61)
     print(ansrad)
     return atan(ansrad) #should work but doesnt - why?
     
