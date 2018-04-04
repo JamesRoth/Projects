cbe639db4f40c25ffd3b60828b15e5bbd62a07e1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 08:31:33 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,1 +1,15 @@
+#James Roth
+#4/4/18
+#hangman.py - graphics hangman
 
+from ggame import *
+
+black=Color(0xffffff,1)
+tan=Color(0xf4b642,1)
+
+blackoutline=LineStyle(1, black)
+
+gallows1=RectangleAsset(100,1000,blackoutline,tan)
+
+Sprite(gallows1,(100,100))
+App().run()
