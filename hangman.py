337254adeab0a3cb5fb6b8b95b3eac933849c653
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 08:37:05 -0400

    Updated from Brython Server: 4/4/2018 8:37:04 AM

--- hangman.py
+++ hangman.py
@@ -10,7 +10,8 @@ brown=Color(0x934b14,1)
 blackoutline=LineStyle(1, black)
 
 gallows1=RectangleAsset(30,1000,blackoutline,brown)
-gallows2=RectangleAsset(200,30,blackoutline,brown)
+gallows2=RectangleAsset(240,30,blackoutline,brown)
+gallows3=LineAsset(200,30,LineStyle=(1,brown))
 
 Sprite(gallows1,(20,50))
 Sprite(gallows2, (40,30))
