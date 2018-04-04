a2a407ef323ae45867b4c988e8879c3a92542b6a
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 14:31:02 -0400

    Updated from Brython Server: 4/4/2018 2:31:00 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -8,11 +8,8 @@ def elev(xl,yl,E,A,Z):
     sina=sin(radians(A))
     cosa=cos(radians(A))
     tane=tan(radians(E))
-    sina=degrees(sina)
-    cosa=degrees(cosa)
-    tane=degrees(tane)
     ansrad=((((yl-sina*Z*tane)**2 + (xl-cosa*Z*tane))**2)**1/2)/(Z-61)
     print(ansrad)
-    return atan(radians(ansrad)) #should work but doesnt - why?
+    return atan(ansrad) #should work but doesnt - why?
     
 print(elev(40.6,-106,77.6,23.1,408))
