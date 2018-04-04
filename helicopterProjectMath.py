825686d925c1cd20f49185ddde8cf81e5ea7dac0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 13:54:34 -0400

    Updated from Brython Server: 4/4/2018 1:54:33 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -8,6 +8,6 @@ def elev(xl,yl,E,A,Z):
     sina=degrees(sin(A))
     cosa=degrees(cos(A))
     tane=degrees(tan(E))
-    return((yl-sina*Z*tane)**2 + (xl-cosa*Z*tane)**2)**1/2)/z-61)
+    return (yl-sina*Z*tane)**2 + (xl-cosa*Z*tane)**2)**1/2)/z-61
     
 print(elev(40.6,-106,77.6,23.1,408))
