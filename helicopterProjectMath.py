71376a5ffc54f9f0b3f1df36e8da881f82d40915
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 20:23:10 -0400

    Updated from Brython Server: 4/4/2018 8:23:10 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -8,13 +8,9 @@ def elev(xl,yl,E,A,Z):
     sina=sin(radians(A))
     cosa=cos(radians(A))
     tane=tan(radians(E))
-    print(sina,cosa,tane,"SPACE")
     p1=round((sina*Z*tane-yl), 4)
     p2=round((cosa*Z*tane-xl),4)
-    print(p1,p2,"SPACE")
-    print(sqrt((p1**2+p2**2)),"SPACE")
     ans=sqrt((p1**2+p2**2))/(Z-61)
-    print(ans)
-    return degrees(atan(ans)) #should work but doesnt - why?
+    return degrees(atan(ans))
     
 print(elev(40.6,-106,77.6,23.1,408))
