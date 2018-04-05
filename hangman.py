380fc128369f4f4e739d6c73274ba5b51ed06ca0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-05 08:45:39 -0400

    Updated from Brython Server: 4/5/2018 8:45:39 AM

--- hangman.py
+++ hangman.py
@@ -21,17 +21,17 @@ def printHangman(length):
     leg2=PolygonAsset([(0,0),(-10,0),(-50,70),(-60,70)],blackoutline,black)
     for i in range(1,length+1):
         if i==1:
-            Sprite(head,(240,120))
+            Sprite(head,(215,120))
         if i==2:
-            Sprite(body, (240,160))
+            Sprite(body, (215,160))
         if i==3:
-            Sprite(arm1, (190,190))
+            Sprite(arm1, (165,190))
         if i==4:
-            Sprite(arm2, (290,190))
+            Sprite(arm2, (265,190))
         if i==5:
-            Sprite(leg1, (240,220))
+            Sprite(leg1, (215,220))
         if i==6:
-            Sprite(leg2, (240,220))
+            Sprite(leg2, (215,220))
 
 def pickWord():
     num=randint(1,7)
