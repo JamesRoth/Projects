b9cbae95595a97872aecb55a9c1374d1b17c7e43
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-05 13:52:31 -0400

    Updated from Brython Server: 4/5/2018 1:52:31 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -14,3 +14,15 @@ def elev(xl,yl,E,A,Z):
     return str(degrees(atan(ans)))+" degrees"
     
 print(elev(40.6,-106,77.6,23.1,408))
+
+def beaconRange(xl,yl,E,A,Z):
+    sina=sin(radians(A))
+    cosa=cos(radians(A))
+    tane=tan(radians(E))
+    p1=round((sina*Z*tane-yl), 4)
+    p2=round((cosa*Z*tane-xl),4)
+    d=sqrt((p1**2+p2**2))
+    ans=sqrt((p1**2+p2**2))/(Z-61)
+    angle=degrees(atan(ans))
+    #sin angle = d/range
+    return (1/sin(radians(angle))*d) + "meters"
