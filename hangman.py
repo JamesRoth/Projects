0202f7aa0b72fa348810ce3ca6e5f3dba19c0bda
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-05 08:11:44 -0400

    Updated from Brython Server: 4/5/2018 8:11:43 AM

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@
 from ggame import *
 from random import randint
 
-def pickWord:
+def pickWord():
     num=randint(1,7)
     if num==1:
         return "apocalypse"
@@ -22,25 +22,25 @@ def pickWord:
     if num==7:
         return "giraffe"
 
-if name == "_main_":
-
-word=pickWord
-
-black=Color(0x000000,1)
-brown=Color(0x934b14,1)
-
-blackoutline=LineStyle(1, black)
-
-gallows1=RectangleAsset(30,1000,blackoutline,brown)
-gallows2=RectangleAsset(240,30,blackoutline,brown)
-gallows3=RectangleAsset(20, 80, blackoutline,black)
-blank=RectangleAsset(70,10,blackoutline,black)
-
-Sprite(gallows1,(20,50))
-Sprite(gallows2, (40,30))
-Sprite(gallows3, (240,60))
-for i in range(1,len(word)+1):
-    Sprite(blank, (150+i*100, 600))
-
-
-App().run()
+if __name__ == "__main__":
+    
+    word=pickWord
+    
+    black=Color(0x000000,1)
+    brown=Color(0x934b14,1)
+    
+    blackoutline=LineStyle(1, black)
+    
+    gallows1=RectangleAsset(30,1000,blackoutline,brown)
+    gallows2=RectangleAsset(240,30,blackoutline,brown)
+    gallows3=RectangleAsset(20, 80, blackoutline,black)
+    blank=RectangleAsset(70,10,blackoutline,black)
+    
+    Sprite(gallows1,(20,50))
+    Sprite(gallows2, (40,30))
+    Sprite(gallows3, (240,60))
+    for i in range(1,len(word)+1):
+        Sprite(blank, (150+i*100, 600))
+    
+    
+    App().run()
