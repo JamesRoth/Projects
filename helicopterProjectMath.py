90a9360d50d94c47e21b73334037aa4c4a4d2376
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-04 20:23:39 -0400

    Updated from Brython Server: 4/4/2018 8:23:39 PM

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -11,6 +11,6 @@ def elev(xl,yl,E,A,Z):
     p1=round((sina*Z*tane-yl), 4)
     p2=round((cosa*Z*tane-xl),4)
     ans=sqrt((p1**2+p2**2))/(Z-61)
-    return degrees(atan(ans))
+    return str(degrees(atan(ans)))+" degrees"
     
 print(elev(40.6,-106,77.6,23.1,408))
