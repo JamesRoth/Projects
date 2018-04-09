ad9efcb0642c6d2040331d15e7092c3618558b07
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-09 08:23:56 -0400

    Updated from Brython Server: 4/9/2018 8:23:55 AM

--- hangman.py
+++ hangman.py
@@ -12,7 +12,7 @@ def wordComplete(): #checking to see if the whole word is guessed
         data["loss"]=2
 
 def guess(event): #when a letter is guessed - is it right or wrong?
-    Sprite(TextAsset(event.key,fill=black,style="25pt Arial"),(500+40*(data["goodGuess"]+data["badGuess"]),50)) # - drawing letters on top of each other
+    Sprite(TextAsset(event.key,fill=black,style="25pt Arial"),((450+25*len(data["goodGuess"]+data["badGuess"])),50)) # - drawing letters on top of each other
     if data["loss"]==0:
         ch=event.key
         if ch not in data["badGuess"] and ch not in word:
