40d5e6aa40569a85642b2b778966813b55e2a99c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-10 14:17:13 -0400

    Update helicopterProjectMath.py

--- helicopterProjectMath.py
+++ helicopterProjectMath.py
@@ -11,7 +11,7 @@ def elev(xl,yl,E,A,Z):
     p1=(sina*Z*tane-yl)
     p2=(cosa*Z*tane-xl)
     ans=sqrt((p1**2+p2**2))/(Z-61)
-    return "ELEV = " + str(round(degrees(atan(ans)),2)) + " degrees"
+    print("ELEV = " + str(round(degrees(atan(ans)),2)) + " degrees")
 
 def azim(xl,yl,E,A,Z):
     sina=sin(radians(A))
@@ -20,12 +20,10 @@ def azim(xl,yl,E,A,Z):
     p1=(sina*Z*tane-yl)
     p2=(cosa*Z*tane-xl)
     ans=sqrt((p1**2+p2**2))
-    if A>270:
-        return "AZIM = " + str(round(360-degrees(acos((p2)/ans)),2)) + " degrees"
-    elif A>180:
-        return "AZIM = " + str(round(180 + (180-degrees(acos((p2)/ans))),2)) + " degrees"
+    if A>180:
+        print("AZIM = " + str(round(360-degrees(acos((p2)/ans)),2)) + " degrees")
     else:
-        return "AZIM = " + str(round(degrees(acos((p2)/ans)),2)) + " degrees"
+        print("AZIM = " + str(round(degrees(acos((p2)/ans)),2)) + " degrees")
 
 def beaconRange(xl,yl,E,A,Z):
     sina=sin(radians(A))
@@ -37,17 +35,17 @@ def beaconRange(xl,yl,E,A,Z):
     ans=sqrt((p1**2+p2**2))/(Z-61)
     angle=degrees(atan(ans))
     #sin angle = d/range
-    return "RANGE = " + str(round(d/sin(radians(angle)),2)) + " meters"
+    print("RANGE = " + str(round(d/sin(radians(angle)),2)) + " meters")
 
-print(elev(40.6,-106,77.6,23.1,408))
-print(elev(40.6,-106,77.9,146.7,408))
-print(elev(40.6,-106,77.5,201.8,408))
-print(elev(40.6,-106,78.0,330.0,408))
-print(azim(40.6,-106,77.6,23.1,408))
-print(azim(40.6,-106,77.9,146.7,408))
-print(azim(40.6,-106,77.5,201.8,408))
-print(azim(40.6,-106,78.0,330.0,408))
-print(beaconRange(40.6,-106,77.6,23.1,408))
-print(beaconRange(40.6,-106,77.9,146.7,408))
-print(beaconRange(40.6,-106,77.5,201.8,408))
-print(beaconRange(40.6,-106,78.0,330.0,408))
+elev(40.6,-106,77.6,23.1,408)
+elev(40.6,-106,77.9,146.7,408)
+elev(40.6,-106,77.5,201.8,408)
+elev(40.6,-106,78.0,330.0,408)
+azim(40.6,-106,77.6,23.1,408)
+azim(40.6,-106,77.9,146.7,408)
+azim(40.6,-106,77.5,201.8,408)
+azim(40.6,-106,78.0,330.0,408)
+beaconRange(40.6,-106,77.6,23.1,408)
+beaconRange(40.6,-106,77.9,146.7,408)
+beaconRange(40.6,-106,77.5,201.8,408)
+beaconRange(40.6,-106,78.0,330.0,408)
