346ef41bbfbb6b29535f1eed3384c8dfe5c10c54
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 11:27:38 -0400

    Updated from Brython Server: 4/26/2018 11:27:38 AM

--- snake.py
+++ snake.py
@@ -8,3 +8,11 @@ from random import randint
 CELLSIZE=20
 ROWS=28
 COLUMNS=52
+
+green=Color(0x006600,1)
+black=Color(0xffffff,1)
+red=Color(0xff0000,1)
+blackOutline=LineStyle(1,black)
+
+if __name__ == "__main__":
+    background=RectangleAsset(1000,700,blackOutline,green)
