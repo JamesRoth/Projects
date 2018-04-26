b7b5ce29bb352b0f38e8c9bdd0549427fd48d93c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 11:30:35 -0400

    Updated from Brython Server: 4/26/2018 11:30:35 AM

--- snake.py
+++ snake.py
@@ -5,16 +5,27 @@
 from ggame import *
 from random import randint
 
+#constants
 CELLSIZE=20
 ROWS=28
 COLUMNS=52
 
-green=Color(0x006600,1)
-black=Color(0xffffff,1)
-red=Color(0xff0000,1)
-blackOutline=LineStyle(1,black)
+#functions
+def 
 
 if __name__ == "__main__":
+    
+    #dictionary
+    data={}
+    data["length"]=0
+    
+    #colors
+    green=Color(0x006600,1)
+    black=Color(0xffffff,1)
+    red=Color(0xff0000,1)
+    blackOutline=LineStyle(1,black)
+
     background=RectangleAsset(CELLSIZE*COLUMNS,CELLSIZE*ROWS,blackOutline,green)
     
+    #spriting the assets
     Sprite(background)
