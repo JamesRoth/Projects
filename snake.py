6d0cf9298f9e1de3c06eb7bdcea4a2ef503cd8fe
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-04-26 11:50:04 -0400

    Updated from Brython Server: 4/26/2018 11:50:04 AM

--- snake.py
+++ snake.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     data["appleX"]=randint(0,ROWS)
     data["appleY"]=randint(0,COLUMNS)
     data["length"]=4
-    data["snakePos"]=[ROWS/2, COLUMNS/2, ROWS/2-1, COLUMNS/2-1, ROWS/2-2, COLUMNS/2-2, ROWS/2-3, COLUMNS/2-3] #initial snake position
+    data["snakePos"]=[ROWS/2-3, COLUMNS/2-3, ROWS/2-2, COLUMNS/2-2, ROWS/2-1, COLUMNS/2-1, ROWS/2, COLUMNS/2] #initial snake position
     
     #colors
     green=Color(0x006600,1)
