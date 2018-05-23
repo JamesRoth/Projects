312c06819a259f77676b55b6474dc2b918442f53
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-23 08:48:33 -0400

    Updated from Brython Server: 5/23/2018 8:48:32 AM

--- snakeActual.py
+++ snakeActual.py
@@ -17,6 +17,7 @@ red=Color(0xff0000,1)
 tan=Color(0xd2b48c,1)
 blackOutline=LineStyle(1,black)
 
+#functions
 def redrawAll(): #clears board
     for item in App().spritelist[:]:
         item.destroy()
@@ -25,5 +26,9 @@ def drawSnakeBoard():
     Sprite(RectangleAsset(CELLSIZE*COLUMNS,CELLSIZE*ROWS,blackOutline,green))
     #Sprite(RectangleAsset(CELLSIZE,CELLSIZE,blackOutline,tan)) - snake cell
 
+def loadSnakeBoard():
+    data["board"] = []
+    for i in range(1, ROWS+1)
+
 if __name__ == "__main__":
-    
+    data = {}
