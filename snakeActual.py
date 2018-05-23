198fbe0aa245be5787c4d7157fc3c602d6063d5e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-23 11:15:38 -0400

    Updated from Brython Server: 5/23/2018 11:15:37 AM

--- snakeActual.py
+++ snakeActual.py
@@ -45,7 +45,7 @@ def drawSnakeCell(): #draws snake and food
             if data["board"][i][j] == -1:
                 Sprite(RectangleAsset(CELLSIZE,CELLSIZE,blackOutline,red),(CELLSIZE*j+1,CELLSIZE*i+1))
 
-def moveSnake(Row, Col):
+def moveSnake(Row, Col): #seems to be creating two more snake parts - but I'm not pressing the arrow keys, the only time its being called?
     data["headY"] += Col
     data["headX"] += Row
     data["board"][data["headY"]][data["headX"]] = data["lenSnake"]
