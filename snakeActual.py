d4fed2ac3764d795de65c6b5771c9a859054e3c3
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-23 08:31:15 -0400

    Updated from Brython Server: 5/23/2018 8:31:14 AM

--- snakeActual.py
+++ snakeActual.py
@@ -2,6 +2,15 @@
 #5/23/18
 #snakeActual.py - snake game for final project
 
+from ggame import *
+from random import randint
+
 def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
+
+green=Color(0x006600,1)
+black=Color(0x000000,1)
+red=Color(0xff0000,1)
+tan=Color(0xd2b48c,1)
+blackOutline=LineStyle(1,black)
