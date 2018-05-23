239997545d1fcdd6b30c329a3841c0304e60e439
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-23 08:37:25 -0400

    Updated from Brython Server: 5/23/2018 8:37:24 AM

--- snakeActual.py
+++ snakeActual.py
@@ -5,12 +5,23 @@
 from ggame import *
 from random import randint
 
-def redrawAll():
-    for item in App().spritelist[:]:
-        item.destroy()
+#constants
+CELLSIZE = 20
+ROWS = 28
+COLUMNS = 40
 
+#colors
 green=Color(0x006600,1)
 black=Color(0x000000,1)
 red=Color(0xff0000,1)
 tan=Color(0xd2b48c,1)
 blackOutline=LineStyle(1,black)
+
+def redrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
+
+spriteAll():
+    Sprite(RectangleAsset(CELLSIZE*COLUMNS,CELLSIZE*ROWS,blackOutline,green))
+    Sprite(RectangleAsset(CELLSIZE,CELLSIZE,blackOutline,tan))
+
