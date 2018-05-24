860158a21510e73e71f6aa1da9c059e10dde5c82
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-24 08:47:47 -0400

    Updated from Brython Server: 5/24/2018 8:47:47 AM

--- snakeActual.py
+++ snakeActual.py
@@ -108,5 +108,5 @@ if __name__ == "__main__":
     App().listenKeyEvent("keydown","up arrow", moveUp)
     App().listenKeyEvent("keydown","down arrow", moveDown)
     
-    App().run(step)
+    App().run()
     
