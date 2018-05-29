8f94fe68d4cacb294461023a5059221385b63189
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-29 08:12:59 -0400

    Updated from Brython Server: 5/29/2018 8:12:59 AM

--- snakeActual.py
+++ snakeActual.py
@@ -78,7 +78,7 @@ def moveSnake(col, row): #updates the matrix with the snake's position
         print("Meal")
     elif data["board"][data["headY"]][data["headX"]] >= 1: #hit yourself?
         gameOver()
-    elif data["headY"] + row > ROWS or data["headY"] + row < -1 or data["headX"] + col > COLUMNS or data["headX"] + col < 0: #hit edge?
+    elif data["headY"] + row > ROWS or data["headY"] + row < -1 or data["headX"] + col > COLUMNS or data["headX"] + col < -1: #hit edge?
         gameOver()
         print("edge" , "Y:", data["headY"] + row > ROWS or data["headY"] + row < 0, "X:", data["headX"] + col > COLUMNS or data["headX"] + col < 0  )
     elif data["board"][data["headY"]][data["headX"]] == 0: #cell empty?
