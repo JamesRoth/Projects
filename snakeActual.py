53d1036ab38a384a808a4bb8d2664a254cdcb095
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-30 09:31:10 -0400

    Updated from Brython Server: 5/30/2018 9:31:10 AM

--- snakeActual.py
+++ snakeActual.py
@@ -85,8 +85,8 @@ def moveSnake(col, row): #updates the matrix with the snake's position
     elif data["board"][data["headY"]][data["headX"]] >= 1: #hit yourself?
         gameOver()
     elif data["headY"] >= ROWS or data["headY"] < 0 or data["headX"] >= COLUMNS or data["headX"] < 0: #hit edge?
-        gameOver()
         print("edge" , "Y:", data["headY"] >= ROWS or data["headY"] < 0, "X:", data["headX"] >= COLUMNS or data["headX"] < 0)
+        gameOver
     elif data["board"][data["headY"]][data["headX"]] == 0: #cell empty?
         removeTail()
         data["board"][data["headY"]][data["headX"]] = data["lenSnake"]
