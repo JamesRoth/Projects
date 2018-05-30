a6d2c4bb5bacb18617388af6930c69c403b14a7e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-30 08:11:14 -0400

    Updated from Brython Server: 5/30/2018 8:11:12 AM

--- snakeActual.py
+++ snakeActual.py
@@ -31,7 +31,7 @@ def gameOver(): #collided with edge/self - ends game
 def drawSnakeBoard(): #draws background, calls snake creation
     drawSnakeCell()
     Sprite(RectangleAsset(CELLSIZE*COLUMNS,CELLSIZE*ROWS,LineStyle(1,black),white)) #border
-    Sprite(TextAsset("Length:", data["lenSnake"], fill = black, style = "50pt Arial"),(CELLSIZE*COLUMNS+100, 20))
+    Sprite(TextAsset("Length: " + str(data["lenSnake"]), fill = black, style = "50pt Arial"),(CELLSIZE*COLUMNS+100, 20))
 
 def step(): #runs game
     data["frames"] += 1
