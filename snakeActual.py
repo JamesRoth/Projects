81a1c82f02c961bf89dc0c6fc3f92f1e1c466f4f
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-06-01 08:56:06 -0400

    Updated from Brython Server: 6/1/2018 8:56:05 AM

--- snakeActual.py
+++ snakeActual.py
@@ -56,7 +56,7 @@ def step(): #runs game
         if data["gameOver"] == 0:
             moveSnake(data["movement"][0], data["movement"][1])
         else:
-            Sprite(TextAsset("Game Over", fill = red, style = "30pt Arial"),(CELLSIZE*(COLUMNS-4)/2,CELLSIZE*(ROWS-4)/2))
+            Sprite(TextAsset("Game Over", fill = red, style = "20pt Arial"),(CELLSIZE*(COLUMNS-4)/2,CELLSIZE*(ROWS-4)/2))
 
 def loadSnakeBoard(): #initial matrix of board
     for i in range(0, ROWS+1):
