a471143eda0d5a2055e8cb07983078a96fc0ec20
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-06-06 08:05:42 -0400

    Updated from Brython Server: 6/6/2018 8:05:41 AM

--- snakeActual.py
+++ snakeActual.py
@@ -60,7 +60,7 @@ def loadSnakeBoard(): #initial matrix of board
     for i in range(0, ROWS+1):
         list1=[]
         for j in range(0, COLUMNS+1):
-            list1.append[0]
+            list1.append(0)
         data["board"].append(list1)
     data["board"][data["headY"]][data["headX"]] = 1 #initial snake position
     placeFood()
