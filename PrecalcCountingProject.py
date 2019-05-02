b4041b8cb15afa05b1bfd6e82474c480470a5a65
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-05-02 12:20:06 -0400

    Updated from Brython Server: 5/2/2019 12:20:05 PM

--- PrecalcCountingProject.py
+++ PrecalcCountingProject.py
@@ -157,7 +157,7 @@ def strat2Theo():
     cost2Theo = partsCostTheo(0,N)
     print("Cost (strat 2): ",cost2Theo)
     
-def strat3Theo(): #REMEMBER COST OF DEFECTIVE PARTS
+def strat3Theo(): #- NEED TO CHECK WITH DONNELLY - THINK MY CALCULATIONS FOR LINE 173 (TOTALCOST) ARE WRONG
     sampleAmount = []
     samplePercentDefective = []
     totalCost = []
