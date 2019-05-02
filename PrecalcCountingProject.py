0aad49dccf098cc0fd2a30899c605aff1d5464f9
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-05-02 12:16:38 -0400

    Updated from Brython Server: 5/2/2019 12:16:37 PM

--- PrecalcCountingProject.py
+++ PrecalcCountingProject.py
@@ -170,7 +170,7 @@ def strat3Theo(): #REMEMBER COST OF DEFECTIVE PARTS AND COST OF SAMPLES
     print(samplePercentDefective)
     #finding the total cost
     for i in range(0,len(sampleAmount)):
-        totalCost.append(round(samplePercentDefective[i]/100*M + partsCostTheo(1, (N*samplePercentDefective[i]/100)) + partsCostTheo(0, (N*(1-samplePercentDefective[i]/100))),3) )
+        totalCost.append(round(samplePercentDefective[i]/100*M + partsCostTheo(1, (N*samplePercentDefective[i]/100)) + partsCostTheo(0, (N*(1-samplePercentDefective[i]/100))),3) + (i+1)*S )
     print("Total cost (strat 3) ", totalCost)
 
 #a new experimental production run
