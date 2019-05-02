53d6a84aa70dea67567fe9535179d7894b352da6
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-05-02 11:52:05 -0400

    Updated from Brython Server: 5/2/2019 11:52:04 AM

--- PrecalcCountingProject.py
+++ PrecalcCountingProject.py
@@ -165,7 +165,7 @@ def strat3Theo():
         sampleAmount.append(i)
     #% of the time the master mechanic will be called for each amount of sample parts
     for i in range(0,len(sampleAmount)):
-        samplePercentDefective.append(sampleAmount[i]*.8*(100-G) + sampleAmount[i]*.15*(100-K) + sampleAmount[i]*.05*(100-W))
+        samplePercentDefective.append(round(sampleAmount[i]*.8*(100-G) + sampleAmount[i]*.15*(100-K) + sampleAmount[i]*.05*(100-W)))
     print(samplePercentDefective)
 
 #a new experimental production run
