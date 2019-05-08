f2a7b7eebd2d8fea57ed9d2ff1937c1d3e80efbc
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-05-08 12:46:31 -0400

    Updated from Brython Server: 5/8/2019 12:46:31 PM

--- PrecalcCountingProject.py
+++ PrecalcCountingProject.py
@@ -211,7 +211,7 @@ def avgStrat3(list):
     avgCostStrat3 = []
     for i in range(0, len(list[0])):
         avgCostStrat3.append(('New set', i+1))
-        for item in list:
+        for item in list[i]:
             sum+=item[i]
             print("For ", i+1, " sample part(s), the average cost is: ", sum/len(list))
             avgCostStrat3.append(round(sum/len(list)))
