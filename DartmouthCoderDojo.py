9d745b5eb39e0d9f166cb1e62842ca7c70806013
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-05-14 10:15:08 -0400

    Updated from Brython Server: 5/14/2019 10:15:07 AM

--- DartmouthCoderDojo.py
+++ DartmouthCoderDojo.py
@@ -3,7 +3,10 @@
 #DartmouthCoderDojo.py - Dartmouth students teaching elements of python
 
 def sumList(A):
-    return A[0] + sumList(A[1:])
+    if len(A) == 0:
+        return 0
+    else:
+        return A[0] + sumList(A[1:])
 
 
 sumList([1,2,3,4,5,6,7,8])
