95a8c116703d0006d7bc4063cb234bc2692974ac
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-11-12 08:37:59 -0500

    Updated from Brython Server: 11/12/2019 8:37:57 AM

--- CalcDerivativesProject.py
+++ CalcDerivativesProject.py
@@ -2,7 +2,12 @@
 #11/12/19
 #Calc derivative project
 
-from scitools import *
+"""
+import scitools.stringfunction
 from scitools.StringFunction import StringFunction
 f = StringFunction('1+sin(2*x)')
 f(1.2)
+"""
+
+#delcaring varialbes
+tolerance = 
