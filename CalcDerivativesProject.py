1c4b9b3b4d7768ee43133d1a34f0b9053a5df905
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-11-14 08:18:41 -0500

    Update CalcDerivativesProject.py

--- CalcDerivativesProject.py
+++ CalcDerivativesProject.py
@@ -19,21 +19,27 @@ def numDeriv(x,h):
     ((x+h)**2 - (x-h)**2)/(2*h)
     return(round((((x+h)**2 - (x-h)**2)/(2*h)),3))
 
-def extremeFinder(stepDeriv,domainLow,domainHigh):
+def finder(stepDeriv,domainLow,domainHigh): #STILL NEED ENPOINT CASE
+    listMin = []
+    listMax = []
     for i in range (0, stepDeriv*abs(domainLow-domainHigh)):
         x = round(domainLow+i/stepDeriv,4)
+        leftDeriv = numDeriv(x-tolerance, tolerance)
+        rightDeriv = numDeriv(x+tolerance,tolerance)
         if numDeriv(x-tolerance, tolerance)*numDeriv(x+tolerance,tolerance) < 0:
             print("Sign change", x)
-
-
+            if leftDeriv > 0:
+                listMax.append(x)
+            elif leftDeriv < 0:
+                listMin.append(x)
 
 def maxFinder():
-    listMax = []
+    print(list)
     print('Abs Max at x=',max(listMax))
     
 def minFinder():
-    listMin = []
     print('Abs Min at x=',min(listMin))
     
 print(numDeriv(0,tolerance))
-extremeFinder(100,-10,10)
+finder(100,-10,10)
+
