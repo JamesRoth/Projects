964c1ca38b4390a956201d08d2fdaf8d14032472
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-11-19 10:31:07 -0500

    Update CalcDerivativesProject.py

--- CalcDerivativesProject.py
+++ CalcDerivativesProject.py
@@ -2,132 +2,13 @@
 #11/12/19
 #Calc derivative project
 
-#all of this is the function interperter, our code starts at line 134
+#all of this is the function interperter, our code starts at line 132
+#imports
 import math
-def int_checker(string):
-    ans=True
-    for x in string:
-        if not(x in set(['1','2','3','4','5','6','7','8','9','0','.',',','-'])):
-            ans=False
-    return ans
-def find_loci(string, sym):
-    paren=0
-    for x in range(len(string)):
-        if string[x]==sym and paren<1:
-            return x
-        elif string[x]=='(':
-            paren+=1
-        elif string[x]==')':
-            paren-=1
-    return False
-def all_in_paren(string):
-    if string[0]=='(' and string[-1]==')' and not('(' in set(string[1:-1]) or '(' in set(string[1:-1])):
-        return True
-    else:
-        return False
-#everything above sets up functions that will be necessary for the one below
-
-def individual_perform(equ):
-    if all_in_paren(equ):
-        return individual_perform(equ[1:-1])
-    elif int_checker(equ)==True:
-        def ans(x):
-            return eval(equ)
-    elif find_loci(equ, '+')!=False:
-        sym_loci=find_loci(equ,'+')
-        pre= equ[:sym_loci]
-        post= equ[sym_loci+1:]
-        def ans(x):
-            return individual_perform(pre)(x)+individual_perform(post)(x)
-    elif find_loci(equ, '-')!=False:
-        sym_loci=find_loci(equ,'-')
-        pre= equ[:sym_loci]
-        post= equ[sym_loci+1:]
-        def ans(x):
-            return individual_perform(pre)(x)-individual_perform(post)(x)
-    elif find_loci(equ, '*')!=False:
-        sym_loci=find_loci(equ,'*')
-        pre= equ[:sym_loci]
-        post= equ[sym_loci+1:]
-        def ans(x):
-            return individual_perform(pre)(x)*individual_perform(post)(x)
-    elif find_loci(equ, '/')!=False:
-        sym_loci=find_loci(equ,'/')
-        pre= equ[:sym_loci]
-        post= equ[sym_loci+1:]
-        def ans(x):
-            return individual_perform(pre)(x)/individual_perform(post)(x)
-    elif find_loci(equ,'^')!=False:
-        sym_loci=find_loci(equ, '^')
-        pre= equ[:sym_loci]
-        post= equ[sym_loci+1:]
-        def ans(x):
-            return individual_perform(pre)(x)**individual_perform(post)(x)
-    elif equ[:3]=='sin':
-        contents=equ[3:]
-        def ans(x):
-            return math.sin(individual_perform(contents)(x))
-    elif equ[:3]=='cos':
-        contents=equ[3:]
-        def ans(x):
-            return math.cos(individual_perform(contents)(x))
-    elif equ[:3]=='tan':
-        contents=equ[3:]
-        def ans(x):
-            return math.tan(individual_perform(contents)(x))
-    elif equ[:3]=='csc':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.sin(individual_perform(contents)(x))
-    elif equ[:3]=='sec':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.cos(individual_perform(contents)(x))
-    elif equ[:3]=='cot':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.tan(individual_perform(contents)(x))
-    elif equ[:3]=='log':
-        end=find_loci(equ,'_')
-        base=equ[3:end]
-        def ans(x):
-            return math.log(x)/math.log(base)
-    elif equ=='e':
-        def ans(x):
-            return math.e
-    elif equ=='pi':
-        def ans(x):
-            return math.pi
-    elif equ[:3]=='asin':
-        contents=equ[3:]
-        def ans(x):
-            return math.asin(individual_perform(contents)(x))
-    elif equ[:3]=='acos':
-        contents=equ[3:]
-        def ans(x):
-            return math.acos(individual_perform(contents)(x))
-    elif equ[:3]=='atan':
-        contents=equ[3:]
-        def ans(x):
-            return math.atan(individual_perform(contents)(x))
-    elif equ[:3]=='acsc':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.asin(individual_perform(contents)(x))
-    elif equ[:3]=='asec':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.acos(individual_perform(contents)(x))
-    elif equ[:3]=='acot':
-        contents=equ[3:]
-        def ans(x):
-            return 1/math.atan(individual_perform(contents)(x))
-    else:
-        def ans(x):
-            return x
-    return ans
-    
-#function_1=individual_perform(func_1)
+from math import sin,cos, tan, acos, asin, atan
+from math import exp, e, pi
+from math import log, log10, sqrt, log2
+from math import ceil, floor
 
 #delcaring varialbes
 tolerance = 0.001
@@ -137,10 +18,11 @@ listDeriv = []
 listPOI = []
 
 #functions
-def f(x): #the function we are currently finding all the info for
-    #func_1='cos(x*pi)+x^3'
-    #return(function_1(x))
-    return x**2
+def f(xVal): #the function we are currently finding all the info for
+    x=xVal
+    return(eval(func))
+    #return (x**3) - (2*x)
+    #return x**2
 
 def numDeriv(x,h): #finds the numerical derivative at a point
     ans = (f(x+h) - f(x-h))/(2*h)
@@ -185,7 +67,7 @@ def finder(stepDeriv,domainLow,domainHigh): #finds all of the information we wan
     listMax = []
     listPOI = [] #has endpoints as well as POIs
     listPOI.append(domainLow)
-    x = round(domainLow) #left endpoint case
+    x = domainLow #left endpoint case
     val = numDerivLeft(x,tolerance) #just using this variable so I only calculate this number once
     if val > 0:
         listMin.append(domainLow)
@@ -193,22 +75,27 @@ def finder(stepDeriv,domainLow,domainHigh): #finds all of the information we wan
         listMax.append(domainLow)
     for i in range (1, stepDeriv*abs(domainLow-domainHigh)-1):
         x = (domainLow+i/stepDeriv)
-        leftDeriv = numDeriv(x-(1/stepDeriv), tolerance)
-        rightDeriv = numDeriv(x+(1/stepDeriv),tolerance)
-        deriv = numDeriv(x,tolerance)
+        leftDeriv = round(numDeriv(x-(1/stepDeriv), tolerance),10)
+        rightDeriv = round(numDeriv(x+(1/stepDeriv),tolerance),10)
+        deriv = round(numDeriv(x,tolerance),10)
         listDeriv.append(numDeriv(x,tolerance))
         #first derivative stuff
-        if deriv*numDeriv(x+(1/stepDeriv),tolerance) < 0 and deriv != 0: #not recognizing both sides of the zero
+        if deriv*rightDeriv < 0 and deriv != 0:
+            #debugging stuff (the print statements)
+            #print(deriv,rightDeriv)
+            #print("Deriv*",deriv*rightDeriv)
+            #print("sign change:", x, deriv)
             if leftDeriv > 0:
                 listMax.append(x)
             elif leftDeriv < 0:
                 listMin.append(x)
         elif deriv == 0:
             if leftDeriv*rightDeriv < 0:
-            if leftDeriv > 0:
-                listMax.append(x)
-            elif leftDeriv < 0:
-                listMin.append(x)
+                print("sign change:", x)
+                if leftDeriv > 0:
+                    listMax.append(x)
+                elif leftDeriv < 0:
+                    listMin.append(x)
         #second derivative stuff
         if numSecDeriv(x, tolerance)*numSecDeriv(x+(1/stepDeriv),tolerance) < 0:
             listPOI.append(x)
@@ -257,9 +144,10 @@ def absMinFinder(listMin): #finds the absolute minimum in a list of all minimums
             print('Absolute min at x =', smallestX)
 
 #intperets the function you enter
-func_1 = input("Enter your function, but please remember proper syntax: When typing the function, don’t include spaces. You also have to stick to certain syntax. ^ is used to bring a number to a power, while * is for multiplication exclusively. I can’t promise it will work if you input commas. Division is done by /, and it includes all trig functions and their inverses (with the inverse notation being asin instead of sin, acsc instead of csc, etc.) When inputting a logarithm, you first type log, then the base, then _, then the number whose log you want to find. Ln isn’t a working command, but the program can recognize both e and pi (typed as seen here).It follows general order of operations (although it recognizes only parentheses, not brackets) but also prioritizes the positive direction of the negative (addition before subtraction, multiplication before division). ")
-function_1=individual_perform(func_1)
+domainL = int(input("Enter the left bound of your domain: "))
+domainH = int(input("Enter the right bound of your domain: "))
+func = input("Input a function: ")
 
-finder(100,-2,2)
+finder(100,domainL,domainH)
 
 
