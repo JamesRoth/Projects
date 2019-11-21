bc7956af32d225e23d9b1d8dd484843516ba6d10
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-11-21 08:26:42 -0500

    Updated from Brython Server: 11/21/2019 8:26:41 AM

--- LRAM,MRAM,RRAM.py
+++ LRAM,MRAM,RRAM.py
@@ -14,9 +14,13 @@ func = input("Input a function: ”)
 intervalLow = int(input("Enter the lower bound of your interval: "))
 intervalHigh = int(input("Enter the higher bound of your interval: "))
 interval = int(input("Enter the interval value: "))
+lRam = 0
+rRam = 0
+mRam = 0
 
 base = (intervalHigh-intervalLow)/interval
 for i in range(0,interval):
     #LRAM
-    f(i/interval)*base
+    lRam += f(i/interval)*base
+    
 
