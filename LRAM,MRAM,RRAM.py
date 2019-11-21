2505b435ccbe40bafe00c2800c56dc20a067c616
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-11-21 08:21:39 -0500

    Updated from Brython Server: 11/21/2019 8:21:38 AM

--- LRAM,MRAM,RRAM.py
+++ LRAM,MRAM,RRAM.py
@@ -1,1 +1,12 @@
+#James Roth
+#11/21/19
+#LRAM, MRAM, RRAM - finding the area under a curve with different estimations
 
+from math import sin, cos, tan, acos, asin, atan
+from math import exp, e, pi
+from math import log, log10, sqrt, log2
+
+def f(x):
+    
+func = input("Input a function: ”)
+eval(func)
