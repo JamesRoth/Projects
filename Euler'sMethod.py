7ce600eba73ee7124de5c665ca690a4cf763d3bb
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-12-19 09:11:30 -0500

    Updated from Brython Server: 12/19/2019 9:11:29 AM

--- Euler'sMethod.py
+++ Euler'sMethod.py
@@ -6,15 +6,25 @@ from math import sin, cos, tan, acos, asin, atan
 from math import exp, e, pi
 from math import log, log10, sqrt, log2
 
+#evaluates functions
 def f(xVal,yVal):
     x=xVal
     y=yVal
     return(eval(func))
 
+#standard run of input statements
 func = input("Enter a function (g(x), or dy/dx: ")
-startX = int(input("Enter the starting x-value: "))
-startY = int(input("Enter the starting y-value: "))
+startX = float(input("Enter the starting x-value: "))
+startY = float(input("Enter the starting y-value: "))
+endX = float(input("Enter the ending x-value: "))
 n = int(input("Enter the n-value you want: "))
+
+#variables:
+deltaX = (endX - startX)/n
+
+
+
+
 
 
 
