e7f028db51d8f5ab79e03bd0ad68f075e1b7739e
jaRometh27 <35492763+JamesRoth@users.noreply.github.com>
2019-12-19 09:00:46 -0500

    Updated from Brython Server: 12/19/2019 9:00:45 AM

--- Euler'sMethod.py
+++ Euler'sMethod.py
@@ -1,1 +1,11 @@
+#James Roth
+#12/19/19
+#Euler'sMethod.py - a program to solve euler's method
 
+from math import sin, cos, tan, acos, asin, atan
+from math import exp, e, pi
+from math import log, log10, sqrt, log2
+
+def f(xVal):
+    x=xVal
+    return(eval(func))
